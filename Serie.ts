eed19036550071c58cab695de1e910d5aa7d63fc
miguess99 <115753654+miguess99@users.noreply.github.com>
2024-04-17 13:42:53 -0500

    primera parte

---
+++ Serie.ts
@@ -0,0 +1,4 @@
+export class Serie {
+    constructor(public id: number, public nombre: string, public striming: string, public temporadas: number, public tipo: string,public paginaWeb: string, public imagenP: string) 
+    {}
+}
---
+++ scripts/Serie.js
@@ -0,0 +1,13 @@
+var Serie = /** @class */ (function () {
+    function Serie(id, nombre, striming, temporadas, tipo, paginaWeb, imagenP) {
+        this.id = id;
+        this.nombre = nombre;
+        this.striming = striming;
+        this.temporadas = temporadas;
+        this.tipo = tipo;
+        this.paginaWeb = paginaWeb;
+        this.imagenP = imagenP;
+    }
+    return Serie;
+}());
+export { Serie };
